ab4b24d00c6d5d1c2eb46376c9bc811a4674c5b0
mrbryantguy <55728391+mrbryantguy@users.noreply.github.com>
2019-11-05 13:34:01 -0700

    Add files via upload

--- RecursiveQuestion.java
+++ RecursiveQuestion.java
@@ -22,11 +22,45 @@ public class RecursiveQuestion {
 		System.out.println("Printing the first 20 Fibonacci numbers.\n");
 		
 		for( int i = 0; i < 20; i++ ) {
-			System.out.println(i + ": " + FibBinaryRecursive(i));
+			System.out.println( i + ": " + FibBinaryRecursive(i) );
 		} // end for
-		
-		//
-		
 	} // end main
+	
+		/*
+		 * This project uses the Towers class from
+		 * Chapter 3’s Programming Project 12 on
+		 * page 171. For the project, write a recursive 
+		 * method that computes and prints a solution to the xxx
+		 * Towers of Hanoi game. The method should meet this specification:
+				public static void Hanoi(
+				Towers t,
+				int n,
+				int start,
+				int target,
+				int spare
+				);
+			// Precondition: start, target, and spare are
+			// three different peg numbers of the game,
+			// and n is non-negative.
+			// Also, the n smallest rings currently are on
+			// the top of the start peg.
+			// Postcondition: The method has activated
+			// a sequence of moves for the tower t, so
+			// that the total effect of these moves is to
+			// shift the top n rings from the start peg to
+			// the target peg, using spare as the extra
+			// peg. None of the other rings has been
+			// disturbed. Each time a move is made,
+			// a message describing the move is
+			// printed.
+		Your solution should have a simple stopping case:
+		When n is zero, there is no work to do. When n is
+		more than zero, use a recursive call to move n–1
+		rings from the start peg to the spare peg. Then call
+		the move method to move one ring from the start peg
+		to the target peg. Finally, make another recursive
+		call to move n–1 rings from the spare peg to the
+		target peg.
+		*/
 	
 } // end RecursiveQuestion
